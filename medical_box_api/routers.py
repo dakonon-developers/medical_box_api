b074a82d615ab1d16648eeb2df68ddca1b767f6c
Pablo Gonzalez <pdonaire1@gmail.com>
2016-04-30 13:52:17 -0430

    Added Some Models

---
+++ medical_box_api/routers.py
@@ -0,0 +1,19 @@
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+# router = SimpleRouter()
+
+
+router = DefaultRouter()
+# ------------------------------------------
+# router.register('cards', CardViewSet, 'card')
+# router.register('categories', CategoryViewSet, 'category')
+# router.register('customers', CustomerViewSet)
+# router.register('flatpages', FlatPageViewSet)
+# router.register('invoices', InvoiceViewSet, 'invoice')
+# router.register('partners', PartnerViewSet)
+# router.register('products', ProductViewSet, 'product')
+# router.register('stores', StoreViewSet)
+# router.register('templates', TemplateViewSet)
+# router.register('files', FileViewSet, 'file')
+# router.register('orders',  OrderViewSet, 'order')
+# router.register('settings',  SettingsViewSet, 'settings')
